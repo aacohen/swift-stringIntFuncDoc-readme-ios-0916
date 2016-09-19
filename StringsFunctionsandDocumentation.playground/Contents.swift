077626ba8a693bdbeab77e6087a4ba501c432c1f
aacohen <arielaades@gmail.com>
2016-09-19 11:00:36 -0400

    Swift StringIntFunc Complete

---
+++ StringsFunctionsandDocumentation.playground/Contents.swift
@@ -0,0 +1,8 @@
+//: Playground - noun: a place where people can play
+
+let name = "Audrey"
+
+name.uppercaseString
+
+let uppercaseName = name.uppercaseString
+print ("\(name), \(name), \(uppercaseName)! Wake up!")
